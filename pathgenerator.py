7bccf8f94d1113eb279f5dd32ddc5ac907c4535b
Jack Swiggett <jackswiggett@gmail.com>
2016-02-14 15:23:57 +0100

    pathgenerator: add decompose_helper() with new functionality
    decompose_helper() can pass the line used to generate the
    decomposition at any angle. It considers both exterior and interior
    points. The next step is to use this functionality in decompose() in
    order to attempt several complete decompositions at different angles,
    and select the best one.

--- pathgenerator.py
+++ pathgenerator.py
@@ -1,64 +1,81 @@
 # This module generates a complete coverage path for a field
 # defined by an arbitrary polygon
 
+import shapely.affinity
 from shapely.geometry import (Point, Polygon,
                               LineString, GeometryCollection,
                               MultiPolygon)
+import pdb
 
-def sign(val):
-    if val >= 0:
-        return 1
-    else:
-        return -1
+def decompose(polygon):
+    return decompose_helper(polygon, angle=-30)
 
-def decompose_polygon(polygon):
-    # copy the polygon, since we will be destructively modifying
+def decompose_helper(polygon, angle=0):
+    """
+    Decomposes the given Polygon into cells that can be easily
+    covered by a simple, "ox-like" path. Returns a list of the
+    decomposed cells (as Polygons).
+
+    In order to generate the boustrophedon decomposition, this
+    function passes a line from left to right through the given
+    Polygon. The angle parameter can be modified in order
+    to pass the line in a different direction (0 <= angle < 365).
+    """
+    # copy the polygon, since we will destructively modify it
     polygon = Polygon(polygon)
 
+    # Rotate. We undo this rotation at the end of the function.
+    rotate_point = polygon.representative_point()
+    polygon = shapely.affinity.rotate(polygon, angle,
+                                      origin=rotate_point)
+
+    # Assemble a list of all interior and exterior vertices
     coords = polygon.exterior.coords
     coords = coords[:-1] # last coord is a repeat of the first
+    if len(coords) <= 2: return [] # this is a line, not a polygon
+    for interior_ring in polygon.interiors:
+        interior_coords = interior_ring.coords
+        interior_coords = interior_coords[:-1]
+        coords += interior_coords
 
     minx, miny, maxx, maxy = polygon.bounds
-    minx -= 1
-    miny -= 1
-    maxy += 1
-    
-    # For now, pass a vertical line from bottom to top through
-    # the polygon
-    reference_line = LineString([(minx, miny), (maxx, miny)])
-    
+    start_line = LineString([(minx - 1, miny - 1),
+                             (minx - 1, maxy + 1)])
+    end_line = LineString([(maxx + 1, miny - 1),
+                           ( maxx + 1, maxy + 1)])
+
+    # Sort the indices so that we consider the vertices closest to
+    # the start line first, and the farthest vertices last. This
+    # creates the effect of passing the line from left to right
+    # through the polygon.
     sorted_indices = range(0, len(coords))
     sorted_indices.sort(
         key=lambda index:
-        Point(coords[index]).distance(reference_line))
-
+        Point(coords[index]).distance(start_line))
+    
     cells = []
     for index in sorted_indices:
         point = Point(coords[index])
-        dist = point.distance(reference_line)
+        dist = point.distance(start_line)
 
-        # Vertices are only critical if the two edges of the
-        # polygon that are connected to that vertex go on the same
-        # side of the reference line when it passes through that
-        # vertex, forming a point that points opposite the
-        # direction of motion of the reference line.
-        #
-        # Exception: the last vertex is always critical.
         prev_index = (index - 1) % len(coords)
         prev_point = Point(coords[prev_index])
-        prev_dist = prev_point.distance(reference_line)
+        prev_dist = prev_point.distance(start_line)
 
         next_index = (index + 1) % len(coords)
         next_point = Point(coords[next_index])
-        next_dist = next_point.distance(reference_line)
+        next_dist = next_point.distance(start_line)
 
-        if ((prev_dist <= dist or next_dist <= dist) and
-            index != sorted_indices[-1]):
-            # This is not a critical point (see comment above)
+        if ((prev_dist < dist and next_dist > dist) or
+            (prev_dist > dist and next_dist < dist)):
+            # A basic ox-like path can pass by this vertex without
+            # a problem (it is a MIDDLE event), so we do not
+            # consider it as a critical point.
             continue
 
-        # Create a new cell
-        box = reference_line.union(point).envelope
+        # Create new cell(s) by slicing the polygon with a vertical
+        # line at this point
+        box = start_line.union(point).envelope
         new_cell = box.intersection(polygon)
         if isinstance(new_cell, Polygon):
             cells.append(new_cell)
@@ -68,6 +85,11 @@ def decompose_polygon(polygon):
                 if isinstance(item, Polygon):
                     cells.append(item)
         
-        polygon = polygon.symmetric_difference(new_cell)
+        remainder_box = end_line.union(point).envelope
+        polygon = polygon.intersection(remainder_box)
     
+    # Undo rotation for all of the cells
+    for i in xrange(0, len(cells)):
+        cells[i] = shapely.affinity.rotate(cells[i], -angle,
+                                           origin=rotate_point)
     return cells
