22bf5258fbcbd1795760384a3c33211f3b26d7cb
jkamalu <john.kamalu@gmail.com>
2016-02-15 13:01:40 +0100

    Commit to push to remote branch

--- MissionGenerator.py
+++ MissionGenerator.py
@@ -1,4 +1,27 @@
-from Command import Command
+class Command:
+
+	""" 
+	Command { [
+		index, 
+		current_waypoint,
+		coordinate_frame, 
+		command_id, 
+		var1 - Hold time in decimal seconds. (ignored by fixed wing, time to stay at MISSION for rotary wing),
+		var2 - Acceptance radius in meters (if the sphere with this radius is hit, the MISSION counts as reached),
+		var3 - 0 to pass through the WP, if > 0 radius in meters to pass by WP. Positive value for clockwise orbit, negative value for counter-clockwise orbit (allows trajectory control),
+		var4 - Desired yaw angle at MISSION (rotary wing),
+		latitude, 
+		longitude, 
+		altitude, 
+		auto_continue 
+	] }
+	
+	"""
+
+	NUM_ATTRIBUTES = 11
+
+	def __init__(self, command_id, latitude, longitude, altitude):
+		self.attributes = [0, 0, command_id, 0, 0, 0, 0, latitude, longitude, altitude, 1]
 
 class MissionGenerator:
 
--- parsepolygon.py
+++ parsepolygon.py
@@ -1,6 +1,21 @@
 from shapely.geometry import Polygon
 
-def parse(file_name):
+def parse_polygon(file_name):
+
+	"""
+	This function parses a file to construct a polygon object.
+
+	Pre-requesite: (1) file_name should reference a .waypoints file and include the file extension e.g. "file_name.waypoints", and (2) the file referenced by file_name should be in the following format, and all bracketed values must throw no exception when input into the float() function.
+
+		|point_id|latitude|longitude|altitude|
+		|<id_1>|<latitude_1>|<longitude_1>|<altitude_1>|
+		...
+		...
+		...
+		|<id_n>|<latitude_n>|<longitude_n>|<altitude_n>|
+
+	Return: a Polygon instance as specified by the python shapely library.
+	"""
 	# list to build coordinates of polygon during creation
 	coords = []
 	file_in = open(file_name, 'r')
@@ -26,6 +41,3 @@ def parse(file_name):
 			break
 	file_in.close()
 	return Polygon(coords)
-
-if __name__ == '__main__':
-	print parse('isep.txt')
