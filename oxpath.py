9afebec60b4793e590d00be8c500d19655a0f637
Jack Swiggett <jackswiggett@gmail.com>
2016-02-21 23:14:32 +0100

    pathgenerator: complete implementation
    Generates an efficient complete coverage path.
    pathgenerator.generate_path() returns a list of the waypoints that
    make up the path, as well as a MissionGenerator object that can be
    immediately used to print the waypoints to a file.

--- oxpath.py
+++ oxpath.py
@@ -1,9 +1,88 @@
 # This module generates an ox-style (boustrophedon) coverage
 # path for a given polygonal cell.
 
-from shapely.geometry import (Polygon, LineString)
+from shapely.geometry import (Polygon, LineString, Point)
 
-def generate_path(polygon, path_radius):
+# TODO: Figure out why I can't "from cellsequencer
+# import SequenceElement"
+import cellsequencer
+
+def traversal_endpoints(polygon, path_radius, x, miny, maxy):
+    line = LineString([(x, miny - 1), (x, maxy + 1)])
+    intersection = line.intersection(polygon)
+
+    # TODO: more intelligent error handling than an assert
+    assert isinstance(intersection, LineString)
+
+    # TODO: better handling of areas where there is not
+    # enough vertical space in the field to safely pass the
+    # drone
+    if intersection.length <= path_radius * 2:
+        point1 = intersection.interpolate(.5,
+                                          normalized=True)
+        point2 = point1
+    else:
+        point1 = intersection.interpolate(path_radius)
+        point2 = intersection.interpolate(intersection.length
+                                          - path_radius)
+
+    if point1.y > point2.y:
+        return (point1, point2)
+    else:
+        return (point2, point1)
+
+def coverage_options(polygon, path_radius):
+    minx, miny, maxx, maxy = polygon.bounds
+    
+    # TODO: better handling of areas where there is not
+    # enough horizontal space in the field to safely pass the
+    # drone
+    xleft = minx + path_radius
+    xcenter = (minx + maxx) / 2
+    if xleft > xcenter:
+        xleft = xcenter
+
+    xright = xleft
+    ntraversals = 1
+    while (xright + path_radius * 2) <= (maxx - path_radius):
+        xright += path_radius * 2
+        ntraversals += 1
+
+    upper_left, lower_left = traversal_endpoints(polygon,
+                                                 path_radius,
+                                                 xleft,
+                                                 miny, maxy)
+    upper_right, lower_right = traversal_endpoints(polygon,
+                                                   path_radius,
+                                                   xright,
+                                                   miny, maxy)
+
+    if ntraversals % 2 == 0:
+        # Drone will end its coverage of this polygon at the same
+        # y-position as where it started
+        return [
+            cellsequencer.SequenceElement(
+                polygon, lower_left, lower_right, False, True),
+            cellsequencer.SequenceElement(
+                polygon, lower_right, lower_left, False, False),
+            cellsequencer.SequenceElement(
+                polygon, upper_left, upper_right, True, True),
+            cellsequencer.SequenceElement(
+                polygon, upper_right, upper_left, True, False)]
+    else:
+        # Drone will end its coverage at the opposite y-position
+        # (i.e. start high, end low, or vice versa)
+        return [
+            cellsequencer.SequenceElement(
+                polygon, lower_left, upper_right, False, True),
+            cellsequencer.SequenceElement(
+                polygon, upper_right, lower_left, True, False),
+            cellsequencer.SequenceElement(
+                polygon, upper_left, lower_right, True, True),
+            cellsequencer.SequenceElement(
+                polygon, lower_right, upper_left, False, False)]
+
+def generate_path(polygon, path_radius, start_top, start_left):
     minx, miny, maxx, maxy = polygon.bounds
     
     top_points = []
@@ -11,43 +90,27 @@ def generate_path(polygon, path_radius):
 
     x = minx + path_radius
     while x <= maxx - path_radius:
-        line = LineString([(x, miny - 1), (x, maxy + 1)])
-        intersection = line.intersection(polygon)
-
-        # TODO: more intelligent error handling than an assert
-        assert isinstance(intersection, LineString)
-        
-        # TODO: better handling of areas where there is not
-        # enough vertical space in the field to safely pass the
-        # drone
-        if intersection.length <= path_radius * 2:
-            point1 = intersection.interpolate(.5,
-                                              normalized=True)
-            point2 = point1
-        else:
-            point1 = intersection.interpolate(path_radius)
-            point2 = intersection.interpolate(intersection.length
-                                          - path_radius)
-        
-        if point1.y > point2.y:
-            top_points.append(point1)
-            bottom_points.append(point2)
-        else:
-            top_points.append(point2)
-            bottom_points.append(point1)
-
+        top, bottom = traversal_endpoints(polygon, path_radius,
+                                          x, miny, maxy)
+        top_points.append(top)
+        bottom_points.append(bottom)
         x += path_radius * 2
 
     waypoints = []
-    top_to_bottom = True
+    top_to_bottom = start_top
+    if start_left:
+        pop_index = 0 # pop leftmost point
+    else:
+        pop_index = -1 # pop rightmost point
+
     for i in range(0, len(top_points)):
         if top_to_bottom:
-            waypoints.append(top_points.pop())
-            waypoints.append(bottom_points.pop())
+            waypoints.append(top_points.pop(pop_index))
+            waypoints.append(bottom_points.pop(pop_index))
             top_to_bottom = False
         else:
-            waypoints.append(bottom_points.pop())
-            waypoints.append(top_points.pop())
+            waypoints.append(bottom_points.pop(pop_index))
+            waypoints.append(top_points.pop(pop_index))
             top_to_bottom = True
 
     return waypoints
